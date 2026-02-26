d114177d6e8bd11125c750ca84921cb8d798e6f2
Sehyeon9071 <qkrtpgus11031210@gmail.com>
2026-02-26 22:50:57 +0900

    2026_02_26_22:50

--- Python/박세현_0226_01.py
+++ Python/박세현_0226_01.py
@@ -1,18 +1,44 @@
-dic = {
-    "학번" : input("학번을 입력하시오. : "),
-    "이름" : input("이름을 입력하시오. : "),
-    "국어" : int(input("국어 점수를 입력하시오. : ")),
-    "영어" : int(input("영어 점수를 입력하시오. : ")),
-    "수학" : int(input("수학 점수를 입력하시오. : "))
-}
+stds = []
+total = 0
 
-kor = dic.get("국어")
-eng = dic.get("영어")
-math = dic.get("수학")
-total = kor + eng + math
-avg = total / 3
+while True:
+    hakbun = input("학번을 입력하시오. : ")
+    if hakbun == "exit":
+        break
 
-dic["총점"] = total
-dic["평균"] = avg
+    std = {
+        "info" : {
+            "학번" : hakbun,
+            "이름" : input("이름을 입력하시오. : ")
+        },
+        "score" : {
+            "국어" : int(input("국어 점수를 입력하시오. : ")),
+            "영어": int(input("영어 점수를 입력하시오. : ")),
+            "수학": int(input("수학 점수를 입력하시오. : "))
+        },
+        "grade" : {
 
-print(dic)
+        }
+    }
+
+    std["grade"]["총점"] = sum(std["score"].values())
+    std["grade"]["평균"] = std["grade"]["총점"] / len(std["score"])
+    total += std["grade"]["평균"]
+    stds.append(std)
+
+if len(stds) > 0:
+    print("              *** 성적표 ***")
+    print("========================================")
+    print("학번   이름   국어   영어   수학   총점   평균")
+    print("========================================")
+
+    for i in stds:
+        print(f"{i['info']['학번']}  {i['info']['이름']}"
+            f"  {i['score']['국어']}    {i['score']['영어']}    {i['score']['수학']}"
+            f"    {i['grade']['총점']}    {i['grade']['평균']}")
+
+    print("========================================")
+    print(f"      학생수 : {len(stds)}      전체 평균 : {round(total / len(stds), 2)}")
+
+else:
+    print("학생 정보가 없습니다.")
--- Python/박세현_0226_02.py
+++ Python/박세현_0226_02.py
@@ -1,1 +1,35 @@
-asd
+stds = []
+grade = 0
+
+while True:
+    std = []
+    hakbun = input("학번을 입력하세요. : ")
+    if hakbun == "exit":
+        break
+
+    name = input("이름을 입력하시오. : ")
+    std.append([hakbun, name])
+
+    kor = int(input("국어 점수를 입력하시오. : "))
+    eng = int(input("영어 점수를 입력하시오. : "))
+    math = int(input("수학 점수를 입력하시오. : "))
+    std.append([kor, eng, math])
+
+    total = sum(std[1])
+    avg = total / len(std[1])
+    grade += avg
+    std.append([total, avg])
+    stds.append(std)
+
+if len(stds) > 0:
+    print("              *** 성적표 ***")
+    print("========================================")
+    print("학번   이름   국어   영어   수학   총점   평균")
+    print("========================================")
+
+    for i in stds:
+        print(f"{i[0][0]}  {i[0][1]}  {i[1][0]}    {i[1][1]}    {i[1][2]}    {i[2][0]}    {i[2][1]}")
+
+    print("========================================")
+    print(f"      학생수 : {len(stds)}      전체 평균 : {round(grade / len(stds), 2)}")
+
